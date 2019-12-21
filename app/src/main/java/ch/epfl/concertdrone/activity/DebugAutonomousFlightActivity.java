52b2e22f93d1da9429f0fb86cff856a5dceed51b
YannHoueix <yann.houeixacid@epfl.ch>
2019-12-21 12:05:06 +0100

    Merge branch 'master' of D:\Proyectos\ConcertDrone with conflicts.

---
+++ app/src/main/java/ch/epfl/concertdrone/activity/DebugAutonomousFlightActivity.java
@@ -0,0 +1,14 @@
+package ch.epfl.concertdrone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DebugAutonomusFlightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug_autonomus_flight);
+    }
+}
